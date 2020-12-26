33fbece69a498e80a5e0982a62e152542d89af65
Mattias Buelens <mattias@buelens.com>
2020-12-26 23:48:54 +0100

    Set HWM to 0 for ReadableStream.from()

--- reference-implementation/lib/abstract-ops/readable-streams.js
+++ reference-implementation/lib/abstract-ops/readable-streams.js
@@ -1459,6 +1459,6 @@ function ReadableStreamFromIterable(asyncIterable) {
     return promiseResolvedWith(returnResult);
   }
 
-  stream = CreateReadableStream(startAlgorithm, pullAlgorithm, cancelAlgorithm);
+  stream = CreateReadableStream(startAlgorithm, pullAlgorithm, cancelAlgorithm, 0);
   return stream;
 }
